c602da53350578c305d2754df5545d3c011bd5ed
Jason Lu <jasonbangbang@gmail.com>
2017-01-24 17:13:34 -0600

    Remove need for interrupts for MPU-6050

--- Arduino/SubController/SubController.ino
+++ Arduino/SubController/SubController.ino
@@ -10,7 +10,6 @@
 
 MPU6050 mpu;
 bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
@@ -31,13 +30,7 @@ void dmpDataReady() {
 }
 
 void setup() {
-    /*
-       Configure the pins for software serial
-    */
     pinMode(3, OUTPUT);
-    /*
-       Start the software serial at a baud rate of 9600.
-    */
     Serial.begin(115200);
     // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -58,11 +51,6 @@ void setup() {
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
         //Serial.println(F("DMP ready! Waiting for first interrupt..."));
@@ -85,47 +73,29 @@ void loop() {
     String input = Serial.readStringUntil('\n');
     int joystickY = SubUtils::getValue(input, ' ', 1).toInt();
     analogWrite(3, joystickY);
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
+    if (!dmpReady) {
+        return;
+    } else {
+        while (fifoCount < packetSize) {
+            fifoCount = mpu.getFIFOCount();
+        }
+        if (fifoCount == 1024) {
+            mpu.resetFIFO();
+        } else {
+            fifoCount = mpu.getFIFOCount();
+            mpu.getFIFOBytes(fifoBuffer,packetSize);
+            mpu.resetFIFO();
 
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
+            fifoCount -= packetSize ;
 
-    }
-
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        //Serial.println(F("FIFO overflow!"));
-
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-
-        // display Euler angles in degrees
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        Serial.print(ypr[2] * 18/M_PI);
-        Serial.print(" ");
-        Serial.print(ypr[1] * 180/M_PI);
-        Serial.print(" ");
-        Serial.println(ypr[0] * 180/M_PI);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print(" ");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print(" ");
+            Serial.println(ypr[2] * 180/M_PI);
+        }
     }
 }
